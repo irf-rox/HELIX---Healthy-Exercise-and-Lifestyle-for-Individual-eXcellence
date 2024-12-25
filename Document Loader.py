4dd3905bcc8a32f5bca80e2a4c50e7374cbe787f
A Irfaan Fareed <94896261+irf-rox@users.noreply.github.com>
2024-12-25 13:17:05 +0530

    Delete Document Loader.py

--- Document Loader.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.document_loaders import DirectoryLoader
-data_path="Dataset"
-
-def doc_loader():
-    loader = DirectoryLoader(data_path, glob="*.csv")
-    documents = loader.load()
-    return documents
